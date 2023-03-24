cc765533bef1eb7e07ef003641e0a62f66513eaa
Sherbek <amirovsherbek1205@gmail.com>
2023-03-24 11:38:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Layout from './Layout/Layout';
+function App() {
+  return (
+    <div className="App">
+           <Layout/>
+    </div>
+  );
+}
+export default App;
---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+function Layout(){
+    return (
+        <div>
+           <header>
+             navbar
+           </header>
+           <main>
+              main body
+           </main>
+           <footer>
+              footer
+           </footer>
+        </div>
+    )
+}
+export default Layout
